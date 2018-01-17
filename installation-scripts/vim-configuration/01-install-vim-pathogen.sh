981f3c095cbb27d28786ed05d2791f55970d5acf
data-pup <kevinmartinucr@gmail.com>
2018-01-16 17:43:47 -0800

    Added script logic to install vim pathogen automatically.

--- installation-scripts/vim-configuration/01-install-vim-pathogen.sh
+++ installation-scripts/vim-configuration/01-install-vim-pathogen.sh
@@ -3,4 +3,12 @@
 # This script is responsible for installing vim's Pathogen package manager.
 # --------------------------------------------------------------------------- #
 
-# TODO ...
+# Move to vim's autoload directory by pushing it onto the directory stack.
+pushd "~/.vim/autoload"
+
+# Download the Pathogen vim script from @tpope's Github into ~/.vim/autoload/
+wget "https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
+
+# Return to the original location.
+popd
+
