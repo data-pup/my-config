92a33c967675c96361eef91ce0f61a63e9601a08
data-pup <kevinmartinucr@gmail.com>
2018-01-16 19:06:48 -0800

    Removed configure top-level script.

--- CONFIGURE-UBUNTU.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# --------------------------------------------------------------------------- #
-# This script will configure a fresh Ubuntu installation using the dotfiles
-# stored throughout this repository. This should only be run once.
-# --------------------------------------------------------------------------- #
-
-### TODO
-
