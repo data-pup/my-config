a2679c9a4565e4a6b14cdeaa34e0717abf266ace
data-pup <kevinmartinucr@gmail.com>
2018-01-16 17:31:19 -0800

    Added a file to enable the uncomplicated firewall, and turn on logging.

---
+++ installation-scripts/03-enable-firewall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------------- #
+# This script enables the Uncomplicated Firewall (ufw) 
+# --------------------------------------------------------------------------- #
+
+# Enable the firewall.
+sudo ufw enable
+
+# Enable logging.
+sudo ufw logging on
+
