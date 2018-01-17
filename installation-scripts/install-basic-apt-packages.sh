881331ae76a5d6ae224540274a1eba509f1448ce
data-pup <kevinmartinucr@gmail.com>
2018-01-16 16:28:31 -0800

    Added a basic version of the basic APT package installation script.

---
+++ installation-scripts/install-basic-apt-packages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------------- #
+# This script will install a list of basic APT packages that I want installed
+# in my development environments. This should be run after completing the
+# initial Ubuntu installation process.
+# --------------------------------------------------------------------------- #
+
+# First, update the package list.
+sudo apt-get update
+
+# Next, install my desired packages.
+sudo apt-get install -y \
+    htop \
+    fish \
+    git \
+    ipython \
+    python3 \
+    ranger \
+    tmux \
+    vim \
+    zsh
+
