b4e7c6228e0c21ab6fab73308a4bcc98ca8d5fe0
data-pup <kevinmartinucr@gmail.com>
2018-01-16 17:12:01 -0800

    Added a run script.

---
+++ RUN.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------------- #
+# This script will configure a fresh Ubuntu installation using the dotfiles
+# stored throughout this repository. This should only be run once.
+# --------------------------------------------------------------------------- #
+
+### TODO
+
