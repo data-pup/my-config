6ce33db5fa491e7512037905a4fe6d9ce1e37557
data-pup <kevinmartinucr@gmail.com>
2018-03-19 14:39:27 -0700

    Added a package for installing my usual tools on OS X.

---
+++ installation-scripts/00-install-basic-brew-packages.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------------- #
+# This script will install a list of basic APT packages that I want installed
+# in my development environments. This should be run after completing the
+# initial Ubuntu installation process.
+# --------------------------------------------------------------------------- #
+
+# First, update the package list.
+brew update
+
+# Next, install my desired packages.
+brew install \
+    htop \
+    fish \
+    ipython \
+    npm \
+    ranger \
+    tmux \
+    tree
+
