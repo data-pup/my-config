34cdb472605ef3c4d05b96e1f4996a7f5a57235d
data-pup <kevinmartinucr@gmail.com>
2018-03-23 08:34:37 -0700

    Added a new script for installing nitrogen, and added a note about how to restore the wallpaper.

---
+++ installation-scripts/misc-scripts/nitrogen-wallpaper-manager.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install nitrogen, a wallpaper manager.
+sudo apt install nitrogen
+
+# # # This command should be placed in the i3 config. Alternatively, this can
+# # # be placed within a startup script, but this can cause problems when using
+# # # tmux etc.
+# nitrogen --restore &
+
