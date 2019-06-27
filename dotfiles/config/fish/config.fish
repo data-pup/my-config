f7561ff5235754e585d27d52634dea36d09ebeb7
data-pup <16364986+data-pup@users.noreply.github.com>
2019-06-27 17:40:31 +0000

    Remap lkj to <ESC> for fish shell

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -4,6 +4,11 @@
 # Set vi mode.
 fish_vi_key_bindings
 
+# Remap `lkj` to <ESC>
+function fish_user_key_bindings
+    bind -M insert lkj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+end
+
 # Use nvim rather than vim.
 alias vim="nvim"
 
