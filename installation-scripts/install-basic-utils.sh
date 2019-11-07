065c88b960aba5d8d1fbdada70334fa412140f0b
data-pup <16364986+data-pup@users.noreply.github.com>
2019-11-07 14:55:26 +0100

    installation script update

--- installation-scripts/install-basic-utils.sh
+++ installation-scripts/install-basic-utils.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-# --------------------------------------------------------------------------- #
-# This script will install a list of basic APT packages that I want installed
-# in my development environments. This should be run after completing the
-# initial Ubuntu installation process.
-# --------------------------------------------------------------------------- #
 
 set -o errexit
 set -o pipefail
@@ -14,6 +9,7 @@ sudo apt-get update
 
 # Next, install my desired packages.
 sudo apt-get install -y \
+    bat \
     curl \
     htop \
     fish \
