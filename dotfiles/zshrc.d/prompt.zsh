fe60af281730abdc3cbf1d4f16632beadb98bea2
data-pup <16364986+data-pup@users.noreply.github.com>
2019-12-03 01:05:11 -0500

    revamp zsh prompt

--- dotfiles/zshrc.d/prompt.zsh
+++ dotfiles/zshrc.d/prompt.zsh
@@ -1,7 +1,44 @@
-# Export personal prompt to add a lil' sparkle sparkle.
-export PS1='___________________________________________
-%F{white}[prev_return:%?][curr_event:%h][curr_priv:%#]
- %F{magenta}%B░▒▓█%F{blue}%B%n@%m%b%F{magenta}%B█▓▒░%F{magenta}%B
-%F{blue}%B[%y]:%F{pink}%B%D{%a %b %d} %D{%I:%M:%S%P}
-%U%F{magenta}%~/%b%k%f %F{pink}%}%u
-:  '
+# Allow parameter expansion/command substitution in prompt.
+setopt PROMPT_SUBST
+
+# 8 Bit Fairyfloss colors.
+white='231'
+black='237'
+yellow='178'
+pink='218'
+purple='141'
+
+# Section contents.
+shell_info='[ret:%?][curr:%h][priv:%#]'
+user_host='%n@%m'
+date_time='%D@%*'
+directory='%2~'
+
+separator_char=""
+
+# Print a separator character given a foreground and background color.
+#
+# Parameters:
+#   $1 - foreground
+#   $2 - background
+function separator {
+  echo "%{%F{$1}%K{$2}%f%k%}"
+}
+
+# Echo a section with a separator, given a function, foreground color,
+# and background color.
+#
+# Parameters:
+#   $1 - contents
+#   $2 - foreground
+#   $3 - background
+#   $4 - separator background
+function section {
+  echo "%B%F{${2}}%K{${3}} ${1} %f%k$(separator $3 $4)%b"
+}
+
+# shell_info_section="$(section "$shell_info" "$black" "$white" "$black")"
+date_time_section="$(section "$date_time" "$black" "$white" "$pink")"
+user_host_section="$(section "$user_host" "$white" "$pink" "$purple")"
+directory_section="$(section "$directory" "$white" "$purple" "default")"
+export PS1='${date_time_section}${user_host_section}${directory_section} '
