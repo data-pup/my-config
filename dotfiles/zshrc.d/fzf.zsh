fa4fc2ffdcaa2581fc3c2619d6b292c566336784
data-pup <16364986+data-pup@users.noreply.github.com>
2019-12-16 09:28:56 -0500

    show commit stat instead of changes in git history fzf

--- dotfiles/zshrc.d/fzf.zsh
+++ dotfiles/zshrc.d/fzf.zsh
@@ -52,7 +52,7 @@ gh() {
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always --stat | head -'$LINES |
   grep -o "[a-f0-9]\{7,\}"
 }
 
